1b6c5c3f252fc95956983f8a1910120d44005d2e
张凌峰 <zhanglingfeng@zhanglingfengdeMacBook-Pro.local>
2020-01-18 22:48:10 -0500

    1048

--- testing/test1.py
+++ testing/test1.py
@@ -18,71 +18,74 @@ import nltk
 # nltk.download('wordnet')
 # =============================================================================
 data = pd.read_csv('g.csv', error_bad_lines=False, encoding = "ISO-8859-1")
-data_text = data[['title']]
+data_text = data[['title','simple description','description for Gccampus']]
 documents = data_text
 
 documents['title'] = documents['title'].fillna('999')
 documents = documents.drop(documents[(documents.title=='999')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Cours de EFPC / CSPS courses')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Cours provenant d\'une autre ministère / Other Government Department courses')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Externals Ressources Links')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Cours de Skillsoft courses')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Delegation of Authority - video')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Blogs')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Cases Studies')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='hq')].index.tolist())
-
 documents = documents.drop(documents[(documents.title=='Video List')].index.tolist())
 
+for i in documents.index:
+    if(' '.join(str(documents['simple description'][i]).split()[:10])==' '.join(str(documents['description for Gccampus'][i]).split()[:10])):
+        documents['simple description'][i]= ""
 
-# stemmer = SnowballStemmer("english")
-# def lemmatize_stemming(text):
-#     return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
-# def preprocess(text):
-#     result = []
-#     for token in gensim.utils.simple_preprocess(text):
-#         if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
-#             result.append(lemmatize_stemming(token))
-#     return result
+documents.to_csv("clean.csv")
 
+documents['combine'] = documents[['title','simple description','description for Gccampus']].apply(lambda x : '{} {} {}'.format(x[0],x[1],x[2]), axis=1)
 
-# doc_sample = documents[documents.index == 4].values[0][0]
-# print('\noriginal document: ')
-# words = []
-# for word in doc_sample.split(' '):
-#     words.append(word)
-# print(words)
-# print('\ntokenized and lemmatized document: ')
-# print(preprocess(doc_sample))
-# processed_docs = documents['title'].map(preprocess)
-# processed_docs = processed_docs[:700]
+documents = documents['combine']
 
+documents.to_csv("combine.csv")
 
-# # Bag of words on the dataset
-# dictionary = gensim.corpora.Dictionary(processed_docs)
+# df.drop('name', axis=1)
 
-# count = 0
-# for k, v in dictionary.iteritems():
-#     print(k, v)
-#     count += 1
-#     if count > 10:
-#         break
-# bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
-
-# lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=3)
+stemmer = SnowballStemmer("english")
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
 
 
-# from gensim.test.utils import datapath
-# # temp_file = datapath("model")
-# # lda_model.save(temp_file)
-# lda_model.save("word2vec.model")
+doc_sample = documents[documents.index == 4].values[0][0]
+print('\noriginal document: ')
+words = []
+for word in doc_sample.split(' '):
+    words.append(word)
+print(words)
+print('\ntokenized and lemmatized document: ')
+print(preprocess(doc_sample))
+processed_docs = documents.map(preprocess)
+print(documents.shape[0])
+processed_docs = processed_docs[:documents.shape[0]]
 
-# print("finished")
+# Bag of words on the dataset
+dictionary = gensim.corpora.Dictionary(processed_docs)
+
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+
+lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=3)
+
+
+from gensim.test.utils import datapath
+
+lda_model.save("word2vec.model")
+
+print("finished")
